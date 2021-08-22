64406b60977fcb52020e1faf6a5d16a6003bce0b
Patrick McCarver <patrick@merklebros.com>
2021-08-22 10:44:10 -0400

    Project: Linting project files

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
-import * as ecypher from './ecypher.js';
+import * as ecypher from "./ecypher.js";
 
 function handleEncypher(e) {
   e.preventDefault();
   const cypher = ecypher.buildCypher("abcdefghijklmnopqrstuvwxyz", "e");
   const message = document.getElementById("encypher-text-area").value;
-  document.getElementById('decypher-text-area').value =
-    ecypher.encypher(message, cypher);
+  document.getElementById("decypher-text-area").value = ecypher.encypher(
+    message,
+    cypher
+  );
 }
 
 function handleDecypher(e) {
   e.preventDefault();
   const cypher = ecypher.buildCypher("abcdefghijklmnopqrstuvwxyz", "e");
   const message = document.getElementById("decypher-text-area").value;
-  document.getElementById('encypher-text-area').value = 
-    ecypher.decypher(message, cypher)
+  document.getElementById("encypher-text-area").value = ecypher.decypher(
+    message,
+    cypher
+  );
 }
 
-document.getElementById('encypher-form').onsubmit = handleEncypher; 
-document.getElementById('decypher-form').onsubmit = handleDecypher; 
+document.getElementById("encypher-form").onsubmit = handleEncypher;
+document.getElementById("decypher-form").onsubmit = handleDecypher;
